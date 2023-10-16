8e5e20b23244e036b6f13e772d6d9c0e847e106f
konrad <knwi@protonmail.com>
2023-10-16 16:31:56 +0200

    readability for _encrypt_byte and encrypt

--- bytenigma/main.py
+++ bytenigma/main.py
@@ -1,22 +1,22 @@
 from util import round_rotation, test
 from base64 import b64decode, b64encode
+from hashlib import sha256
 
 
-def encrypt_byte(byte, rotor_array):
-    rotors = len(rotor_array)
+def _encrypt_byte(byte, rotor_array):
     tmp = byte
-    for i in range(rotors):
-        tmp = rotor_array[i][tmp]
+    for rotors in rotor_array:
+        tmp = rotors[tmp]
     tmp ^= 255
-    for i in range(rotors-1,-1,-1):
-        tmp = rotor_array[i].index(tmp)
+    for rotors in reversed(rotor_array):
+        tmp = rotors.index(tmp)
     return tmp.to_bytes(1, byteorder='big')
 
 def encrypt(byte_string, rotor_array):
     result = b''
     tmp_rotor_array = rotor_array
     for i in byte_string:
-        result += encrypt_byte(i, tmp_rotor_array)
+        result += _encrypt_byte(i, tmp_rotor_array)
         tmp_rotor_array = round_rotation(tmp_rotor_array)
     return result
 
@@ -27,5 +27,6 @@ if __name__ == "__main__":
     print(p)
     print(c)
     print(encrypt(p, test["rotors"]))
+    print(sha256(encrypt(b'\x00'*(2**20), test["rotors"])).hexdigest())
 
 
