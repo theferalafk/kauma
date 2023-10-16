101b39b718a8409b249eaed6ec0049dde7a3b018
konrad <knwi@protonmail.com>
2023-10-16 14:54:07 +0200

    added encryption for one byte

--- bytenigma/main.py
+++ bytenigma/main.py
@@ -1,2 +1,22 @@
+from util import round_rotation, test
+from base64 import b64decode, b64encode
+
+
+def encrypt_byte(byte, rotor_array):
+    rotors = len(rotor_array)
+    tmp = int.from_bytes(byte, byteorder='big')
+    for i in range(rotors):
+        tmp = rotor_array[i][tmp]
+    tmp ^= 255
+    for i in range(rotors-1,-1,-1):
+        tmp = rotor_array[i].index(tmp)
+    return tmp.to_bytes(1, byteorder='big')
+
 if __name__ == "__main__":
-    print("yo")
+    p = b64decode("RGFzIGlzdCBlaW4gVGVzdC4=")
+    c = b64decode("lDEjvQHsKWD9c+dHIW++KRo=")
+    print(p)
+    print(c)
+    print(encrypt_byte(b'D', test["rotors"]))
+
+
