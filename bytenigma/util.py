63c1782496b5c894cc24b3e7b305b2675de72fa1
konrad <knwi@protonmail.com>
2023-10-16 14:05:21 +0200

    added rotation of array

---
+++ bytenigma/util.py
@@ -0,0 +1,14 @@
+from base64 import b64decode, b64encode
+
+def rotation(rotation):
+    result = rotation[1:]
+    carry = rotation[0]
+    result.append(carry)
+    return [result, carry]
+
+
+
+if __name__ == "__main__":    
+    a = [1,2,3,4,5,6,7,0]
+    print(rotation(a))
+    print(b64decode("RGFzIGlzdCBlaW4gVGVzdC4="))
