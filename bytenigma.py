a2ab56fae30f4426c9f32998309c5b9166575bf7
Konrad <knwi@protonmail.com>
2023-10-26 17:19:50 +0200

    added documentation

--- bytenigma.py
+++ bytenigma.py
@@ -2,17 +2,22 @@ from util import round_rotation
 
 
 def _encrypt_byte(byte, rotor_array):
+    #takes a byte and a rotor list as an input and encrypts it
     tmp = byte
+    #forward mapping
     for rotors in rotor_array:
         tmp = rotors[tmp]
     tmp ^= 255
+    #reverse mapping
     for rotors in reversed(rotor_array):
         tmp = rotors.index(tmp)
     return tmp.to_bytes(1, byteorder='big')
 
 def encrypt(byte_string, rotor_array):
+    #takes a byte string and a rotor configuration as an input and return the encrypted byte string
     result = []
     tmp_rotor_array = rotor_array
+    #encrypting input string byte by byte
     for i in byte_string:
         result.append(_encrypt_byte(i, tmp_rotor_array))
         tmp_rotor_array = round_rotation(tmp_rotor_array)
--- util.py
+++ util.py
@@ -1,18 +1,24 @@
 def _rotation(rotation):
+    #takes a list as an input and rotates it left, the left most element gets appended most right
+    #return the new array and the former left most element
     result = rotation[1:]
     carry = rotation[0]
     result.append(carry)
     return [result, carry]
 
 def round_rotation(rotor_array):
+    #takes a list of rotors as an input and returns a new list for the next round
     result = []
+    #first rotor has to be rotated
     carried_byte = 0
     for i in range(len(rotor_array)):
+        #checks if rotor needs to be rotated
         if carried_byte == 0:
             tmp = _rotation(rotor_array[i])
             result.append(tmp[0])
             carried_byte = tmp[1]
         else:
+            #no rotation
             result.append(rotor_array[i])
             carried_byte = 1
     return result
