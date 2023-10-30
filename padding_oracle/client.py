2e01ca609ed0edc1bc3ded35e3388f3dbb27b0ba
konrad <knwi@protonmail.com>
2023-10-30 15:04:23 +0100

    refactored client sender into a funktion

--- padding_oracle/client.py
+++ padding_oracle/client.py
@@ -4,15 +4,22 @@ from crypto_until import encrypt, pad
 host = "127.0.0.1"
 port = 3874
 
-a = 13
+def send_oracle_protocol(host, port, n_blocks, ct, iv_list):
+    res = b''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.send(ct)
+        s.send(n_blocks.to_bytes(2, byteorder='little'))
+        for iv in iv_list:
+            s.send(iv)
+        res = s.recv(n_blocks)
+        s.close()
+    return res
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    iv = b'\x0f\xf0'*8
-    ct = b'hallogehtdas'
-    s.send(encrypt(iv, pad(ct)))
-    s.send(a.to_bytes(2, byteorder='little'))
-    for i in range(a):
-        s.send(i.to_bytes(1, byteorder='little')*16)
-    print(s.recv(a))
-    s.close()
+if __name__ == "__main__":
+    ct = encrypt(b'\x12\x34'*8, b'chacha20ply1305')
+    iv_list = []
+    n = 10
+    for i in range(n):
+        iv_list.append(b'12356789012345'+i.to_bytes(1, byteorder='little'))
+    print(send_oracle_protocol("127.0.0.1", 3874, n, ct, iv_list))
