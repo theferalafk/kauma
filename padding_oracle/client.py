216cbbbe2cb7fb0deeb70440dba306578b366d2f
konrad <knwi@protonmail.com>
2023-10-30 14:35:58 +0100

    added server and client for oracle protocol

--- padding_oracle/client.py
+++ padding_oracle/client.py
@@ -1,9 +1,18 @@
 import socket
+from crypto_until import encrypt, pad
 
 host = "127.0.0.1"
 port = 3874
 
+a = 13
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    s.send(b'thihihihihihi')
+    iv = b'\x0f\xf0'*8
+    ct = b'hallogehtdas'
+    s.send(encrypt(iv, pad(ct)))
+    s.send(a.to_bytes(2, byteorder='little'))
+    for i in range(a):
+        s.send(i.to_bytes(1, byteorder='little')*16)
+    print(s.recv(a))
     s.close()
--- padding_oracle/server.py
+++ padding_oracle/server.py
@@ -1,26 +1,32 @@
 import socket
-from cryptography.hazmat.primitives.padding import PKCS7
+from crypto_until import unpad, encrypt, BLOCK_SIZE
+
 
 HOST = "127.0.0.1"
 PORT = 3874
 
-def encrypt():
-    return 0
-
-def decrypt():
-    return 0
-
-def pad():
-    return 0
-
-def unpad():
-    return 0
-
+    
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
         with conn:
-            data = conn.recv(1024)
-            print(data)
+            ct = conn.recv(BLOCK_SIZE)
+            length = int.from_bytes(conn.recv(2), byteorder='little')
+            iv_list = []
+            for i in range(length):
+                iv_list.append(conn.recv(BLOCK_SIZE))
+            response = []
+            for iv in iv_list:
+                #hier muss noch error handling wenn blockgröße nicht erreicht ist
+                print(iv, ct)
+                padded_pt = encrypt(iv, ct)
+                print(padded_pt)
+                if unpad(padded_pt):
+                    response.append(b'\x01')
+                else:
+                    response.append(b'\x00')
+            conn.send(b''.join(response))
+            conn.close()
+            
