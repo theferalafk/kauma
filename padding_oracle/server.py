8849009954084ccb8590a217345b010218bcef3c
Konrad <knwi@protonmail.com>
2023-11-04 15:13:17 +0100

    updated imports from crypto_util so aes is used

--- padding_oracle/server.py
+++ padding_oracle/server.py
@@ -1,37 +1,36 @@
 import socket
-from crypto_util import unpad, encrypt, BLOCK_SIZE
+from crypto_util import unpad, decrypt, BLOCK_SIZE
 
 
 HOST = "127.0.0.1"
 PORT = 3874
 
-    
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #schönere funktion finden
-    s.bind((HOST, PORT))
-    s.listen()
-    while True:
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #schönere funktion finden
         print("running")
-        conn, addr = s.accept()
-        with conn:
-            ct = conn.recv(BLOCK_SIZE)
-            length = int.from_bytes(conn.recv(2), byteorder='little')
-            iv_list = []
-            for i in range(length):
-                iv_list.append(conn.recv(BLOCK_SIZE))
-            response = []
-            for iv in iv_list:
-                #hier muss noch error handling wenn blockgröße nicht erreicht ist
-                padded_pt = encrypt(iv, ct)
-                result = unpad(padded_pt)
-                if result or result==b'':
-                    response.append(b'\x01')
-                    print("received iv: ", iv)
-                    print("received ct: ", ct)
-                    print("padded: ", padded_pt)
-                    print("unpadded: ", unpad(padded_pt))
-                else:
-                    response.append(b'\x00')
-            conn.send(b''.join(response))
-            conn.close()
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                ct = conn.recv(BLOCK_SIZE)
+                length = int.from_bytes(conn.recv(2), byteorder='little')
+                iv_list = []
+                for i in range(length):
+                    iv_list.append(conn.recv(BLOCK_SIZE))
+                response = []
+                for iv in iv_list:
+                    #hier muss noch error handling wenn blockgröße nicht erreicht ist
+                    padded_pt = decrypt(iv, ct)
+                    result = unpad(padded_pt)
+                    if result or result==b'':
+                        response.append(b'\x01')
+                        print("received iv: ", iv)
+                        print("received ct: ", ct)
+                        print("padded: ", padded_pt)
+                        print("unpadded: ", unpad(padded_pt))
+                    else:
+                        response.append(b'\x00')
+                conn.send(b''.join(response))
             
