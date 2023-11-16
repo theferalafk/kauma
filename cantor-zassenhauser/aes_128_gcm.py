fa566cc7bce208a1422561dd6850d36ccd1261db
konrad <knwi@protonmail.com>
2023-11-16 23:11:39 +0100

    added basic de/encryption

--- cantor-zassenhauser/aes_128_gcm.py
+++ cantor-zassenhauser/aes_128_gcm.py
@@ -1,14 +1,20 @@
 from gf import GF, GFElement
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 class AES_128_GCM:
 
-    def aes_ecb(pt):
-        pass
+    def _aes_ecb_encrypt(self, pt):
+        encryptor = self.cipher.encryptor()
+        return encryptor.update(pt) + encryptor.finalize()
+    
+    def _aes_ecb_decrypt(self, ct):
+        decryptor = self.cipher.decryptor()
+        return decryptor.update(ct) + decryptor.finalze()
 
     def __init__(self, key):
         self.key = key
-        self.auth_key = self.aes_ecb("Nullstream")    
+        self.auth_key = self._aes_ecb_encrypt(b'\x00'*16)
+        self.cipher = Cipher(algorithms.AES(key), modes.ECB())
 
     def ghash(ad, ct):
         pass
