12ab898fe980176d40654dca952b024aaada9d34
Konrad <knwi@protonmail.com>
2023-11-25 00:04:09 +0100

    work in progress

--- aes_gcm/poly.py
+++ aes_gcm/poly.py
@@ -53,12 +53,10 @@ class Poly:
         a = self.poly
         b = _b.poly
         summands = []
-        len(a)
-        print("calc on ", a, b)
         for i, element_a in enumerate(a):
             # summanden werden hier berechnet
             tmp = []
-            for j, element_b in enumerate(b):
+            for element_b in b:
                 tmp.append(GFElement(element_a)*GFElement(element_b))
             summands.append([[]]*i + tmp)
         #hier müssen die dann aufaddiert werden und dann kommt res raus
@@ -66,6 +64,23 @@ class Poly:
         for summand in summands:
             res += Poly(summand)
         return res
+    
+ 
+    def __divmod__(self, _b):
+        a = self.poly
+        a_len = len(a)
+        b = _b.poly
+        res = []
+        remainder = []
+        #while len(a)>len(b) -> a[-1] / b[-1] * a[-1] -> res; a-res von eben
+        #remainder ist dann das was übrig bleibt   
+
+        #while len(a)>
+        pass 
+
+
+
+
 
 
 k={
@@ -96,15 +111,19 @@ for key in ["a","b","result"]:
     # b = x^2[3, 7, 9] + x^1[12] + x^0[2, 5, 9, 10]
     # c = x^2[3, 8] + x^1[6, 12] + x^0[2, 9, 10]
 #print(Poly.b64_to_list(k["a"]))
-print(Poly(Poly.b64_to_list(k["b"]))+Poly(Poly.b64_to_list(k["a"])))
-print((Poly(Poly.b64_to_list(k["b"]))*Poly(Poly.b64_to_list(k["a"]))).poly)
+a = Poly(Poly.b64_to_list(k["a"]))
+b = Poly(Poly.b64_to_list(k["b"]))
+print("a = ",a.poly)
+print("b = ",b.poly)
+print(a+b)
+print("c = ", (b*a).poly)
+print(divmod(a,b))
 '''
 F.<x>=GF(2^128)
 R.<X>= PolynomialRing(F)
-a = F.gen()
-b = (a^9+a^8+a^7)*X^2 + (a^6)*X + a^5
-c = (a^9+a^7+a^3)*X^2 + (a^12)*X + (a^2+a^5+a^9+a^10)
-t = X^2*(a^7+a^8+a^9) + X^1*(a^6) + X^0*(a^5)
-b+c
-
+a = (x^9+x^8+x^7)*X^2 + (x^6)*X + x^5
+b = (x^9+x^7+x^3)*X^2 + (x^12)*X + (x^2+x^5+x^9+x^10)
+t = X^2*(x^7+x^8+x^9) + X^1*(x^6) + X^0*(x^5)
+b*c
+t^3
 '''
