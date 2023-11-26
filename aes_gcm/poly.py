3be2a492b534b0493a1d21c64ace16497f0b8f41
konrad <knwi@protonmail.com>
2023-11-27 00:05:43 +0100

    wip

--- aes_gcm/poly.py
+++ aes_gcm/poly.py
@@ -57,6 +57,8 @@ class Poly:
             # summanden werden hier berechnet
             tmp = []
             for element_b in b:
+                print("MUUUUL", GFElement(element_a).element, GFElement(element_b).element)
+                print(GFElement(element_a)*GFElement(element_b))
                 tmp.append(GFElement(element_a)*GFElement(element_b))
             summands.append([[]]*i + tmp)
         #hier müssen die dann aufaddiert werden und dann kommt res raus
@@ -67,16 +69,39 @@ class Poly:
     
  
     def __divmod__(self, _b):
-        a = self.poly
-        a_len = len(a)
-        b = _b.poly
+        a = list(reversed(self.poly))
+        a_degree = len(a)
+        b = list(reversed(_b.poly))
+        b_degree = len(b)
         res = []
         remainder = []
+        #saves the temporary state while performing devision
+        tmp = Poly(self.poly)
+
         #while len(a)>len(b) -> a[-1] / b[-1] * a[-1] -> res; a-res von eben
         #remainder ist dann das was übrig bleibt   
-
-        #while len(a)>
-        pass 
+        if a_degree < b_degree:
+            return (Poly([]), self)
+        
+        for i, poly in enumerate(a):
+            #check if current exp is set
+            if poly == []:
+                continue
+            #check if division is already done
+            if b_degree > a_degree-i:
+                break
+            #GFElement of GF(2^128) pow(126) is its inverse because of fermat
+            #print(poly, GFElement(b[0]).pow(126).element)
+            dividend = GFElement(poly)
+            divisor = GFElement(b[0]).pow(2**128-2)
+            print("a/b", dividend.element, "\t\t",divisor.element)
+            quotient = dividend * divisor
+            print(i, quotient)
+            res.append(quotient)
+            tmp = tmp + ( _b * Poly([quotient]) )
+        print("res, ", list(reversed(res)))
+        print("remainder ", tmp.poly)
+        return tmp
 
 
 
@@ -113,17 +138,25 @@ for key in ["a","b","result"]:
 #print(Poly.b64_to_list(k["a"]))
 a = Poly(Poly.b64_to_list(k["a"]))
 b = Poly(Poly.b64_to_list(k["b"]))
-print("a = ",a.poly)
-print("b = ",b.poly)
-print(a+b)
-print("c = ", (b*a).poly)
-print(divmod(a,b))
+#print("a = ",a.poly)
+#print("b = ",b.poly)
+#print(a+b)
+#print("c = ", (b*a).poly)
+#print(divmod(a,b))
+#print(divmod(a,b).poly)
+c = a*a
+gf = GF()
+print(a.poly)
+print(c.poly)
+print(c.poly)
+print("result is: ", divmod(a,b))
+#print((GFElement([3,7,9])*GFElement([3,7,9]).pow(2**128-2)))
 '''
 F.<x>=GF(2^128)
 R.<X>= PolynomialRing(F)
 a = (x^9+x^8+x^7)*X^2 + (x^6)*X + x^5
 b = (x^9+x^7+x^3)*X^2 + (x^12)*X + (x^2+x^5+x^9+x^10)
 t = X^2*(x^7+x^8+x^9) + X^1*(x^6) + X^0*(x^5)
-b*c
+b*a
 t^3
 '''
