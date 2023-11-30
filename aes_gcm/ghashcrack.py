82e03597982d47063d72d7337f0b111a3ca1f031
Konrad <knwi@protonmail.com>
2023-11-30 10:04:57 +0100

    comments

--- aes_gcm/ghashcrack.py
+++ aes_gcm/ghashcrack.py
@@ -2,10 +2,15 @@ from aes_gcm.aes_128_gcm import AES_128_GCM
 from aes_gcm.cantor_zassenhaus import CZ
 from aes_gcm.poly import Poly
 from aes_gcm.gcm_util import GFElement
+
 class CrackMsg:
+    #helper class for GHashCrack
+    def _slice_msg(msg):
+        return [msg[i:i+16] for i in range(0, len(msg), 16)]
+
     def __init__(self, ct, ad, tag):
-        self.ct = [ct[i:i+16] for i in range(0, len(ct), 16)]
-        self.ad = ad
+        self.ct = CrackMsg._slice_msg(ct)
+        self.ad = CrackMsg._slice_msg(ad)
         self.tag = tag
 
 class GHashCrack:
@@ -17,18 +22,20 @@ class GHashCrack:
         self.h = False
         self.y0 = False
 
-    def _gen_mask(self, tag, msg_blocks, h):
+    def _ghash_xor(self, tag, msg_blocks, h):
+        #performs ghash for msg block under hash key h 
         ghash = AES_128_GCM._ghash(msg_blocks, h)
         return AES_128_GCM._byte_xor(ghash, tag)
     
     def construct_poly(self):
-
+        #constructs two polys with msg1, msg2 of form ad0*X^n + ad1*X^(n-1) + ... ct0*X^(n-i) ... + LField*X + Tag and adds them
         msg1_l = [(len(self.msg1.ad)*8).to_bytes(8, byteorder='big')+(len(self.msg1.ct)*8).to_bytes(8, byteorder='big')]
         msg2_l = [(len(self.msg2.ad)*8).to_bytes(8, byteorder='big')+(len(self.msg2.ct)*8).to_bytes(8, byteorder='big')]
 
-        ad_1 = [self.msg1.ad]
-        ad_2 = [self.msg2.ad]
+        ad_1 = self.msg1.ad[::-1]
+        ad_2 = self.msg2.ad[::-1]
 
+        #avoid leading zeros if ad is 0
         if self.msg1.ad == b'' or self.msg1.ad == b'\x00'*16:
             ad_1 = []
         if self.msg2.ad == b'' or self.msg2.ad == b'\x00'*16:
@@ -46,11 +53,12 @@ class GHashCrack:
         return False
 
     def crack(self):
+        #tries to factor poly using cantor zassenhaus
         cz = CZ(self.construct_poly())
         candidates = cz.factor_poly()
         for i in candidates:
             to_verify = AES_128_GCM._slice_and_combine(self.msg1.ad, b''.join(self.msg1.ct))
-            mask = self._gen_mask(self.msg1.tag, to_verify, GFElement(i[0]))
+            mask = self._ghash_xor(self.msg1.tag, to_verify, GFElement(i[0]))
             if self.verify_mask(mask,GFElement(i[0])):
                 self.y0 = mask
                 self.h = GFElement(i[0])
@@ -58,5 +66,8 @@ class GHashCrack:
         return True
     
     def forge_auth_tag(self, ad, ct):
+        #contructs a tag for given ad and ct, only works after crack() is used
+        if self.h and self.y0:
+            return False
         ghash_msg = AES_128_GCM._slice_and_combine(ad,ct)
-        return self._gen_mask(self.y0, ghash_msg, self.h)
+        return self._ghash_xor(self.y0, ghash_msg, self.h)
